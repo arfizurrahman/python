f731c48b7cbbbbd00f83933401bdceb4874ad430
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-05-10 13:46:18 +0600

    Initial commit

---
+++ Python Basics/datatypes.py
@@ -0,0 +1,14 @@
+# Fundamental data types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# User defined
+# Classes
+
+# Specialized Data Types
---
+++ Python Basics/numbers.py
@@ -0,0 +1,3 @@
+print(type(2 + 4))
+print(2 - 4)
+print(2 / 4)
